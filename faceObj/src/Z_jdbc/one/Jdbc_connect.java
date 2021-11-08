ca33ab6c5245094770760cc3a2976aa2bc6db85e
KuaiJL <908515364@qq.com>
2021-11-08 08:52:16 +0800

    learn collections

--- faceObj/src/Z_jdbc/one/Jdbc_connect.java
+++ faceObj/src/Z_jdbc/one/Jdbc_connect.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Jdbc_connect {
     static final String jdbcUrl = "jdbc:mysql://localhost/bishe?useSSL=false&characterEncoding=utf8";
@@ -14,17 +15,20 @@ public class Jdbc_connect {
     static final String jdbcPassword = "134679";
 
     public static void main(String[] args) throws SQLException {
-        List<User> users = queryUsers();
+        Scanner scanner = new Scanner(System.in); // 创建Scanner对象
+        System.out.print("Input your name: "); // 打印提示
+        String name = scanner.nextLine(); // 读取一行输入并获取字符串
+        List<User> users = queryUsers(name);
         users.forEach(System.out::println);
     }
 
-    static List<User> queryUsers() throws SQLException {
+    static List<User> queryUsers(String name) throws SQLException {
         List<User> users = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword)) {
             try (PreparedStatement ps = conn
-                    .prepareStatement("SELECT * FROM users WHERE user_id=? AND user_name=?")) {
-                ps.setInt(1, 3); // 第一个参数grade=?
-                ps.setString(2, "rafa"); // 第二个参数score=?
+                    .prepareStatement("SELECT * FROM users WHERE user_name=?")) {
+                //ps.setInt(1, 3); // 第一个参数grade=?
+                ps.setString(1, name); // 第二个参数score=?
                 try (ResultSet rs = ps.executeQuery()) {
                     while (rs.next()) {
                         users.add(extractRow(rs));
--- faceObj/src/g_Collection_/MAP_/Map_.java
+++ faceObj/src/g_Collection_/MAP_/Map_.java
@@ -6,21 +6,33 @@ import java.util.Map;
 
 public class Map_ {
     public static void main(String[] args){
-        Map map = new HashMap();  //定义Map集合对象
-        map.put("apple","新鲜的苹果");  //向集合中添加对象
-        map.put("computer","配置优良的计算机");
-        map.put("book","堆积成山的图书");
-        Collection values = map.values();  //获取Map集合的value集合
-        System.out.println(values.getClass());
-        for(Object object:values){
-            System.out.println("键值:"+object.toString());  //输出键值对象
+        {
+            Map<String, String> map = new HashMap();  //定义Map集合对象
+            String s0 = map.put("apple", "新鲜的苹果");  //向集合中添加对象
+            System.out.println(s0);//返回null
+            map.put("computer", "配置优良的计算机");
+            map.put("book", "堆积成山的图书");
+            //原来关联的value对象"堆积成山的图书"就被“冲掉”了。实际上，put()方法的签名是V put(K key, V value)，
+            // 如果放入的key已经存在，put()方法会返回被删除的旧的value，否则，返回null。
+            String s = map.put("book", "堆积成山的图书1");
+            System.out.println(s);
+            Collection values = map.values();  //获取Map集合的value集合
+            System.out.println(values.getClass());
+            for (Object object : values) {
+                System.out.println("键值:" + object.toString());  //输出键值对象
+            }
+            System.out.println("---------------------------");
+            map.put("banana", "香蕉");
+            map.put("huluobu", "胡萝卜");
+            System.out.println(values);
+            System.out.println("---------------------------");
+            values.remove("新鲜的苹果");
+            System.out.println(map);
         }
         System.out.println("---------------------------");
-        map.put("banana","香蕉");
-        map.put("huluobu","胡萝卜");
-        System.out.println(values);
-        System.out.println("---------------------------");
-        values.remove("新鲜的苹果");
-        System.out.println(map);
+        System.out.println("蒯金亮".hashCode());
+        for (int i = 0; i < 16; i++) {
+            System.out.println(("蒯金亮"+i+"py").hashCode()& 0xf);
+        }
     }
 }
